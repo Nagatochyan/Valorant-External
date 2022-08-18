0f76b6ba2b9327fe3ed853b0b7300b4432f6f337
Chase <70964202+Chase1803@users.noreply.github.com>
2022-08-18 23:29:00 +0530

    Update driver.hpp

--- Valorant/Driver/driver.hpp
+++ Valorant/Driver/driver.hpp
@@ -8,18 +8,38 @@ private:
 
 #define DRIVER_READVM				0x80000001
 #define DRIVER_GETPOOL				0x80000002
+#define DRIVER_MOUSE				0x80000003
 
 	int _processid;
 	uint64_t _guardedregion;
 
-	struct _requests {
+	struct _requests
+	{
+		//rw
 		uint32_t    src_pid;
 		uint64_t    src_addr;
 		uint64_t    dst_addr;
 		size_t        size;
+
+		//function requests
 		int request_key;
-		std::uintptr_t allocation;
+
+		//guarded regions
+		uintptr_t allocation;
+
+		//mouse
+		long x;
+		long y;
+		unsigned short button_flags;
 	};
+	
+	auto readvm(uint32_t src_pid, uint64_t src_addr, uint64_t dst_addr, size_t size) -> void
+	{
+		if (src_pid == 0 || src_addr == 0) return;
+
+		_requests out = { src_pid, src_addr, dst_addr, size, DRIVER_READVM };
+		NtUserGetPointerProprietaryId(reinterpret_cast<uintptr_t>(&out));
+	}
 
 public:
 	auto initdriver(int processid) -> void
@@ -30,14 +50,6 @@ public:
 			printf("NtUserGetPointerProprietaryId: %p\n", NtUserGetPointerProprietaryId);
 			_processid = processid;
 		}
-	}
-
-	auto readvm(uint32_t src_pid, uint64_t src_addr, uint64_t dst_addr, size_t size) -> void
-	{
-		if (src_pid == 0 || src_addr == 0) return;
-
-		_requests out = { src_pid, src_addr, dst_addr, size, DRIVER_READVM };
-		NtUserGetPointerProprietaryId(reinterpret_cast<uintptr_t>(&out));
 	}
 
 	auto guarded_region() -> uintptr_t
@@ -91,6 +103,23 @@ public:
 		}
 
 		return buffer;
+	}
+
+	auto move_mouse(long x, long y) -> void
+	{
+		_requests out = { 0 };
+		out.x = x;
+		out.y = y;
+		out.request_key = DRIVER_MOUSE;
+		NtUserGetPointerProprietaryId(reinterpret_cast<uintptr_t>(&out));
+	}
+
+	auto send_input(unsigned short button) -> void
+	{
+		_requests out = { 0 };
+		out.button_flags = button;
+		out.request_key = DRIVER_MOUSE;
+		NtUserGetPointerProprietaryId(reinterpret_cast<uintptr_t>(&out));
 	}
 };
 
